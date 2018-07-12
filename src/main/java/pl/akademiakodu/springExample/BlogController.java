ec55a4656d13c2cf923960a0a3acafd284808039
Paweł Kuźma <pawelkuzma74@gmail.com>
2018-07-12 14:51:29 +0200

    blogcontroler

--- src/main/java/pl/akademiakodu/springExample/BlogController.java
+++ src/main/java/pl/akademiakodu/springExample/BlogController.java
@@ -2,13 +2,31 @@ package pl.akademiakodu.springExample;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
 
     @GetMapping("/")
     public String add(){
+
         return "add";
     }
-}
+
+    @GetMapping("/show")
+    public String show(@RequestParam String title,
+                       @RequestParam String description,
+                       @RequestParam String autor,
+                       ModelMap modelMap
+                       ){
+        post post = new post(title,description, autor);
+        modelMap.put("post",post);
+        return "show";
+
+
+    }
+
+
+    }
--- src/main/java/pl/akademiakodu/springExample/post.java
+++ src/main/java/pl/akademiakodu/springExample/post.java
@@ -1,4 +1,51 @@
 package pl.akademiakodu.springExample;
 
 public class post {
+
+    private  String title;
+    private  String description;
+    private  String autor;
+
+    public post(){}
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+
+    public post(String title, String description, String autor) {
+
+        this.title = title;
+        this.description = description;
+        this.autor = autor;
+    }
+
+    public post(String title) {
+        this.title = title;
+    }
+
+    public String toString() {
+        return getAutor()+" "+getTitle()+" "+getDescription();
+    }
 }
